e40cc726ba81fb96192ad58a8394e9829ad23d0a
opilane <kylli@merefilter.ee>
2024-11-12 11:27:32 +0200

    Feature branch first commit

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,14 @@
 public class Person {
+    public static void main(String[] args) {
+
+        System.out.println("Hello people!");
+        String text = "New world";
+        System.out.println(text);
+
+        int number =15;
+        System.out.println(number);
+
+    }
 }
 
 
